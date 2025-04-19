561329b78e101818f096f941dcdba7124c66a8ce
Ben Greenawald <bgreenawald@gmail.com>
2025-04-19 12:16:40 -0400

    Add heap documentation

--- src/heap.ts
+++ src/heap.ts
@@ -1,61 +1,133 @@
 import DateFile from "src/dateFile";
 
+/**
+ * Min-heap data structure for DateFile objects, keeping only the most recent N items by date.
+ * Used to efficiently track the top N most recent files for each time interval in the plugin.
+ */
 export default class DateMinHeap {
+	/** The heap array storing DateFile objects. */
 	heap: DateFile[];
+	/** The maximum number of items the heap will retain. */
 	size: number;
+
+	/**
+	 * Initialize a new DateMinHeap.
+	 * @param size Maximum number of items to keep in the heap.
+	 */
 	constructor(size: number) {
 		this.heap = [];
 		this.size = size;
 	}
 
-	// Helper Methods
-	getLeftChildIndex(parentIndex: number) {
+	/**
+	 * Get the index of the left child of a given parent index.
+	 * @param parentIndex Index of the parent node.
+	 * @returns Index of the left child node.
+	 */
+	getLeftChildIndex(parentIndex: number): number {
 		return 2 * parentIndex + 1;
 	}
-	getRightChildIndex(parentIndex: number) {
+	/**
+	 * Get the index of the right child of a given parent index.
+	 * @param parentIndex Index of the parent node.
+	 * @returns Index of the right child node.
+	 */
+	getRightChildIndex(parentIndex: number): number {
 		return 2 * parentIndex + 2;
 	}
-	getParentIndex(childIndex: number) {
+
+	/**
+	 * Get the index of the parent of a given child index.
+	 * @param childIndex Index of the child node.
+	 * @returns Index of the parent node.
+	 */
+	getParentIndex(childIndex: number): number {
 		return Math.floor((childIndex - 1) / 2);
 	}
-	hasLeftChild(index: number) {
+
+	/**
+	 * Check if a node at a given index has a left child.
+	 * @param index Index to check.
+	 * @returns True if the node has a left child.
+	 */
+	hasLeftChild(index: number): boolean {
 		return this.getLeftChildIndex(index) < this.heap.length;
 	}
-	hasRightChild(index: number) {
+
+	/**
+	 * Check if a node at a given index has a right child.
+	 * @param index Index to check.
+	 * @returns True if the node has a right child.
+	 */
+	hasRightChild(index: number): boolean {
 		return this.getRightChildIndex(index) < this.heap.length;
 	}
-	hasParent(index: number) {
+
+	/**
+	 * Check if a node at a given index has a parent.
+	 * @param index Index to check.
+	 * @returns True if the node has a parent.
+	 */
+	hasParent(index: number): boolean {
 		return this.getParentIndex(index) >= 0;
 	}
-	leftChild(index: number) {
+
+	/**
+	 * Get the left child DateFile of a node at a given index.
+	 * @param index Index of the parent node.
+	 * @returns The left child DateFile.
+	 */
+	leftChild(index: number): DateFile {
 		return this.heap[this.getLeftChildIndex(index)];
 	}
-	rightChild(index: number) {
+
+	/**
+	 * Get the right child DateFile of a node at a given index.
+	 * @param index Index of the parent node.
+	 * @returns The right child DateFile.
+	 */
+	rightChild(index: number): DateFile {
 		return this.heap[this.getRightChildIndex(index)];
 	}
-	parent(index: number) {
+
+	/**
+	 * Get the parent DateFile of a node at a given index.
+	 * @param index Index of the child node.
+	 * @returns The parent DateFile.
+	 */
+	parent(index: number): DateFile {
 		return this.heap[this.getParentIndex(index)];
 	}
 
 	// Functions to create Min Heap
 
-	swap(indexOne: number, indexTwo: number) {
+	/**
+	 * Swap two nodes in the heap by their indices.
+	 * @param indexOne Index of the first node.
+	 * @param indexTwo Index of the second node.
+	 */
+	swap(indexOne: number, indexTwo: number): void {
 		const temp = this.heap[indexOne];
 		this.heap[indexOne] = this.heap[indexTwo];
 		this.heap[indexTwo] = temp;
 	}
 
-	peek() {
+	/**
+	 * Get the top (oldest) DateFile in the heap without removing it.
+	 * @returns The top DateFile, or null if the heap is empty.
+	 */
+	peek(): DateFile | null {
 		if (this.heap.length === 0) {
 			return null;
 		}
 		return this.heap[0];
 	}
 
-	// Removing an element will remove the
-	// top element with highest priority then
-	// heapifyDown will be called
-	remove() {
+	/**
+	 * Remove and return the top (oldest) DateFile from the heap.
+	 * @returns The removed DateFile, or null if the heap is empty.
+	 */
+	remove(): DateFile | null {
 		if (this.heap.length === 0) {
 			return null;
 		}
@@ -66,8 +138,12 @@ export default class DateMinHeap {
 		return item;
 	}
 
-	add(item: DateFile) {
-
+	/**
+	 * Add a DateFile to the heap, keeping only the most recent N by date.
+	 * If the heap is full and the new item is more recent than the oldest, it replaces the oldest.
+	 * @param item The DateFile to add.
+	 */
+	add(item: DateFile): void {
 		if (this.heap.length < this.size) {
 			this._add(item);
 		} else if (item.date && this.peek() && this.heap[0].date) {
@@ -77,12 +153,20 @@ export default class DateMinHeap {
 			}
 		}
 	}
-	_add(item: DateFile) {
+
+	/**
+	 * Internal helper to add an item and restore heap property upwards.
+	 * @param item The DateFile to add.
+	 */
+	_add(item: DateFile): void {
 		this.heap.push(item);
 		this.heapifyUp();
 	}
 
-	heapifyUp() {
+	/**
+	 * Restore the heap property upwards from the last element.
+	 */
+	heapifyUp(): void {
 		let index = this.heap.length - 1;
 		while (this.hasParent(index) && this.parent(index).date && this.heap[index].date && this.parent(index).date > this.heap[index].date) {
 			this.swap(this.getParentIndex(index), index);
@@ -90,7 +174,10 @@ export default class DateMinHeap {
 		}
 	}
 
-	heapifyDown() {
+	/**
+	 * Restore the heap property downwards from the root element.
+	 */
+	heapifyDown(): void {
 		let index = 0;
 		while (this.hasLeftChild(index)) {
 			let smallerChildIndex = this.getLeftChildIndex(index);
@@ -106,7 +193,11 @@ export default class DateMinHeap {
 		}
 	}
 
-	printHeap() {
+	/**
+	 * Return a string representation of the heap for debugging.
+	 * @returns A string listing the basenames of files in the heap.
+	 */
+	printHeap(): string {
 		let heap = ` ${this.heap[0]} `;
 		for (let i = 1; i < this.heap.length; i++) {
 			heap += ` ${this.heap[i].file.basename} `;
@@ -114,6 +205,7 @@ export default class DateMinHeap {
 		return heap;
 	}
 }
+
 
 DateMinHeap.prototype.toString = function () {
 	return this.heap.toString();
